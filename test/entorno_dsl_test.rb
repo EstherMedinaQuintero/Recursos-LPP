7c6c5be6555b80eea09650cc5682597133cf706d
Esther M. Quintero <alu0101434780@ull.edu.es>
2024-01-07 22:26:21 +0000

    Arreglada prueba dsl

--- test/entorno_dsl_test.rb
+++ test/entorno_dsl_test.rb
@@ -18,37 +18,30 @@ class EntornosDslTest < Test::Unit::TestCase
 
 
   def test_entornos_dsl_resultados
-      
-    # Creamos los recursos
-    recurso2 = Recursos::Ejercicio.new(2, "ULL", "El reino de las variables", "Variables", Recursos::BEGINNER, "guiada", "desenchufada", "30 minutos", "papel", [:algoritmos])
-    recurso3 = Recursos::Ejercicio.new(3, "ULL", "Kidsort: de cifra en cifra", "Bucles", Recursos::BEGINNER, "guiada", "desenchufada", "30 minutos", "papel", [:algoritmos])
-    recurso15 = Recursos::Ejercicio.new(15, 'ULL', "Exploradores de secuencias", 'Secuencias', Recursos::BEGINNER, :guiada, :desenchufada, "30 minutos", "papel", [:algoritmos])
-    recurso21 = Recursos::Ejercicio.new(21, 'ULL', "¡Alerta! condicionales", 'Condicionales', Recursos::BEGINNER, :guiada, :desenchufada, "30 minutos", "papel", [:algoritmos])
-    recurso22 = Recursos::Ejercicio.new(22, 'ULL', "La búsqueda de Camelot...", 'Funciones', Recursos::BEGINNER, :guiada, :desenchufada, "30 minutos", "papel", [:algoritmos])
 
     # Creamos los array de loris
-    loris1 = [5, 5, 3, 1, 3, 5, 2, 3]
+    loris1 = 
 
     # e1 ~ (001, ’Vestibulo’, :taller, [d15, d3, d2, d21, d22]), 0,0
     entorno_dsl_1 = Recursos::EntornosDSL.new("Vestibulo") do
       # Registramos los recursos
-      resgistrar_recurso recurso15
-      resgistrar_recurso recurso3
-      resgistrar_recurso recurso2
-      resgistrar_recurso recurso21
-      resgistrar_recurso recurso22
+      resgistrar_recurso Recursos::Ejercicio.new(15, 'ULL', "Exploradores de secuencias", 'Secuencias', Recursos::BEGINNER, :guiada, :desenchufada, "30 minutos", "papel", [:algoritmos])
+      resgistrar_recurso Recursos::Ejercicio.new(3, "ULL", "Kidsort: de cifra en cifra", "Bucles", Recursos::BEGINNER, "guiada", "desenchufada", "30 minutos", "papel", [:algoritmos])
+      resgistrar_recurso Recursos::Ejercicio.new(2, "ULL", "El reino de las variables", "Variables", Recursos::BEGINNER, "guiada", "desenchufada", "30 minutos", "papel", [:algoritmos])
+      resgistrar_recurso Recursos::Ejercicio.new(21, 'ULL', "¡Alerta! condicionales", 'Condicionales', Recursos::BEGINNER, :guiada, :desenchufada, "30 minutos", "papel", [:algoritmos])
+      resgistrar_recurso Recursos::Ejercicio.new(22, 'ULL', "La búsqueda de Camelot...", 'Funciones', Recursos::BEGINNER, :guiada, :desenchufada, "30 minutos", "papel", [:algoritmos])
       # Evaluamos los recursos
-      evaluar_recurso recurso15, loris1
-      evaluar_recurso recurso3, loris1
-      evaluar_recurso recurso2, loris1
-      evaluar_recurso recurso21, loris1
-      evaluar_recurso recurso22, loris1
+      evaluar_recurso 15, [5, 5, 3, 1, 3, 5, 2, 3]
+      evaluar_recurso 3, [5, 5, 3, 1, 3, 5, 2, 3]
+      evaluar_recurso 2, [5, 5, 3, 1, 3, 5, 2, 3]
+      evaluar_recurso 21, [5, 5, 3, 1, 3, 5, 2, 3]
+      evaluar_recurso 22, [5, 5, 3, 1, 3, 5, 2, 3]
       # Manejamos los temas
-      manejar_tema recurso15
-      manejar_tema recurso3
-      manejar_tema recurso2
-      manejar_tema recurso21
-      manejar_tema recurso22
+      manejar_tema 15
+      manejar_tema 3
+      manejar_tema 2
+      manejar_tema 21
+      manejar_tema 22
     end
 
     # Imprimimos los entornos
